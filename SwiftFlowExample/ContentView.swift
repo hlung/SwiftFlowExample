5c39d3cc2a3f20485bb5257dc14402d7d6fbeca7
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-17 22:46:58 +0800

    Update to version 1.0.0

--- SwiftFlowExample/ContentView.swift
+++ SwiftFlowExample/ContentView.swift
@@ -6,14 +6,15 @@ struct ContentView: View {
 
     let graph = Graph()
 
+    // setup configs
     var blueConfig = NodeConfig()
     blueConfig.backgroundColor = UIColor(red: 0.81, green: 0.96, blue: 1.00, alpha: 1.00)
-
     var redConfig = NodeConfig()
     redConfig.backgroundColor = UIColor(red: 1.00, green: 0.80, blue: 0.82, alpha: 1.00)
 
     graph.nodeConfig = blueConfig
 
+    // add flows to graph
     graph.addFlow([
       Node("Start", shape: .pill),
       Arrow(.down),
@@ -30,16 +31,14 @@ struct ContentView: View {
       Node("Cry", shape: .rect, config: redConfig), // different color using config
       Arrow(.down),
       Node("Go home", shape: .rect, id: "home"),
-    ])
-
-    graph.addFlow([
-      NodeShortcut(id: "home"),
       ArrowLoopBack(from: .bottom, to: .right),
       NodeShortcut(id: "end")
     ])
 
     let graphView = GraphView()
-    graphView.layoutMargins = UIEdgeInsets(top: 20, left: 50, bottom: 20, right: 50)
+    graphView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
+    // draw the graph!
     try! graphView.draw(graph)
 
     return UIViewCenteringWrapper(contentView: graphView)
